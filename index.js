3524647bfa7629d90040171b4d859413eced6836
thisdotLolu <davidebe63@gmail.com>
2023-11-25 12:03:00 +0100

    feat: google auth with passportjs

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express')
-
 const app = express()
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
 
-app.get('/',(req,res)=>{
-    res.send({hi:'there'})
-})
+
+passport.use(new GoogleStrategy({
+    clientID:keys.googleClientID,
+    clientSecret:keys.googleClientSecret,
+    callbackURL:'/auth/google/callback'
+},(accessToken,refreshToken,profile,done)=>{
+    console.log(accessToken)
+    console.log(profile)
+    console.log(done)
+}))
+
+
+app.get('/auth/google',passport.authenticate('google',{
+    scope:['profile','email']}))
+
+app.get('/auth/google/callback',passport.authenticate('google'))
 
 app.listen(5000)
+
